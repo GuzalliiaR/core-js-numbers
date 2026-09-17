f0a4a072745369a351b2f36183c10f0c14f7d541
ramazanova_guzalliia <ramazanova_guzalliia@mail.ru>
2026-09-17 14:56:02 +0500

    feat: task 4

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,13 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const movingX = x2 - x1;
+  const movingY = y2 - y1;
+
+  const mowingL = Math.sqrt(movingX ** 2 + movingY ** 2);
+
+  return mowingL;
 }
 
 /** 5
