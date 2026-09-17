a73eb5c22f297ecbd732ad4b152f7d9cdd0813ce
ramazanova_guzalliia <ramazanova_guzalliia@mail.ru>
2026-09-17 23:59:36 +0500

    feat: task 17

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -349,8 +349,15 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  let number = num;
+  while (number > 1) {
+    number /= 2;
+  }
+
+  if (number === 1) return true;
+
+  return false;
 }
 
 /** 18
