8c48a47bdbfb3964cb0b2bf507e13f58f802fd39
ramazanova_guzalliia <ramazanova_guzalliia@mail.ru>
2026-09-18 00:50:56 +0500

    feat: task 26

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -492,8 +492,8 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /** 27
