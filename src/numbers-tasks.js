6dc7d0b2e7fd18673887646424d502bd44d9eef8
ramazanova_guzalliia <ramazanova_guzalliia@mail.ru>
2026-09-18 13:05:06 +0500

    feat: task 35

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -644,8 +644,9 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  const randomNum = Math.random() * (max - min) + min;
+  return Math.round(randomNum);
 }
 
 /** 36
